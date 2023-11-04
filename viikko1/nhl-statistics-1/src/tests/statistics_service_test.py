c16e2a91fb27ef95f3c0efb7c506fffa8237db13
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-11-04 13:50:34 +0200

    task1.16: finish tests for 100% statservice coverage

--- viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -22,3 +22,28 @@ class TestStatisticsService(unittest.TestCase):
         player_list = self.stats._players
 
         self.assertEqual(len(player_list), 5)
+
+    def test_search_returns_first_player_with_search_term_in_name(self):
+        search_result = self.stats.search("ur")
+
+        self.assertEqual(search_result, self.stats._players[2])
+
+    def test_search_returns_none_if_no_player_name_fits_search_term(self):
+        search_result = self.stats.search("lol")
+
+        self.assertEqual(search_result, None)
+
+    def test_team_returns_players_of_team_correctly(self):
+        players_of_team = self.stats.team("EDM")
+
+        self.assertEqual(len(players_of_team), 3)
+        self.assertEqual(players_of_team[0], self.stats._players[0])
+        self.assertEqual(players_of_team[1], self.stats._players[2])
+        self.assertEqual(players_of_team[2], self.stats._players[4])
+
+    def test_top_returns_correct_amount_of_players_with_most_points_in_order(self):
+        top_players = self.stats.top(1)
+
+        self.assertEqual(len(top_players), 2)
+        self.assertEqual(top_players[0], self.stats._players[4])
+        self.assertEqual(top_players[1], self.stats._players[1])
