1e4f64cf1185b8cdcb14c42368293913d57fef82
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-11-11 15:18:42 +0200

    task2.2: get_project parses toml, creates an object and displays it

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -10,7 +10,15 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        parsed_poetry_content = toml.loads(content)['tool']['poetry']
+
+        project_name = parsed_poetry_content['name']
+        project_description = parsed_poetry_content['description']
+        project_dependencies = parsed_poetry_content['dependencies']
+        project_dev_dependencies = parsed_poetry_content['group']['dev']['dependencies']
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(project_name,
+                       project_description,
+                       project_dependencies,
+                       project_dev_dependencies)
