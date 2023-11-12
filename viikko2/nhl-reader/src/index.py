9cc4b20e98c875befdfcb9713ac8cc5a9c5c7f50
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-11-12 21:01:31 +0200

    task2.12: remove debug prints

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,17 +5,12 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
         if player_dict['nationality'] == 'FIN':
             player = Player(player_dict)
             players.append(player)
-
-    print("Oliot:")
 
     players_by_score = sorted(
         players, key=lambda player: player.score, reverse=True)
