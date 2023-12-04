9e6ef6523adda7a5b69e4d544c38342a256c2c37
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-12-04 19:46:56 +0200

    task5.3: refactor: modularize string forming into own functions

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -31,28 +31,39 @@ class TennisGame:
     def game_is_even(self):
         return self.raw_score1 == self.raw_score2
 
-    def get_score(self):
-        if self.game_is_over():
-            if self.raw_score1 > self.raw_score2:
-                winning_player = self.player1_name
-            else:
-                winning_player = self.player2_name
-            return f"Win for {winning_player}"
+    def display_winner(self):
+        if self.raw_score1 > self.raw_score2:
+            winning_player = self.player1_name
+        else:
+            winning_player = self.player2_name
+        return f"Win for {winning_player}"
 
-        elif self.is_game_point():
-            if self.raw_score1 > self.raw_score2:
-                leading_player = self.player1_name
-            else:
-                leading_player = self.player2_name
-            return f"Advantage {leading_player}"
+    def display_advantage(self):
+        if self.raw_score1 > self.raw_score2:
+            leading_player = self.player1_name
+        else:
+            leading_player = self.player2_name
+        return f"Advantage {leading_player}"
 
-        elif self.game_is_even():
-            if self.raw_score1 < 3:
-                return self.tennis_scores.get(self.raw_score1) + "-All"
-            else:
-                return "Deuce"
+    def display_even_score(self):
+        if self.raw_score1 < 3:
+            return self.tennis_scores.get(self.raw_score1) + "-All"
+        else:
+            return "Deuce"
 
+    def display_ordinary_score(self):
         player1_final_score = self.tennis_scores.get(self.raw_score1)
         player2_final_score = self.tennis_scores.get(self.raw_score2)
         return f"{player1_final_score}-{player2_final_score}"
 
+    def get_score(self):
+        if self.game_is_over():
+            return self.display_winner()
+
+        elif self.is_game_point():
+            return self.display_advantage()
+
+        elif self.game_is_even():
+            return self.display_even_score()
+
+        return self.display_ordinary_score()
