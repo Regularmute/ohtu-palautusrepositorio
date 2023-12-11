d570982d245dd89ca251cda05f45c14ba3e92693
regularmute <93884347+Regularmute@users.noreply.github.com>
2023-12-11 14:24:57 +0200

    task6.2: add matcher for HasFewerThan

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -40,3 +40,13 @@ class Not:
         if self.matcher.test(player):
             return False
         return True
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
